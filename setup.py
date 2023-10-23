23b119891c4cfd5a1427656dbf7c012460fabce7
Daniel Jung <proggy@mailbox.org>
2023-10-23 21:57:42 +0200

    make cythonize work in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
+import numpy
 
 extensions = [
-    Extension('misc', sources=[os.path.join('src', 'tightbinding', 'misc.pyx')]),
-    Extension('sc.core', sources=[os.path.join('src', 'tightbinding', 'sc', 'core.pyx')]),
+    Extension('misc', sources=[os.path.join('src', 'misc.pyx')], include_dirs=[numpy.get_include()]),
+    Extension('sc.core', sources=[os.path.join('src', 'sc', 'core.pyx')], include_dirs=[numpy.get_include()]),
 ]
 
 setup(
+    #package_dir={'cython_test': ''},
     ext_modules=cythonize(extensions, language_level=3)
 )
